78a85c2ae4e4a45b18ec7d9124be4433724bdc46
dinesh-gaire <132206290+dinesh-gaire@users.noreply.github.com>
2024-05-25 07:11:17 +0000

    data fetched from database

--- app/edit-form/[formId]/page.jsx
+++ app/edit-form/[formId]/page.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+"use client"
+import { db } from '@/configs'
+import { JsonForms } from '@/configs/schema'
+import { useUser } from '@clerk/nextjs'
+import { and, eq } from 'drizzle-orm'
+import React, { useEffect, useState } from 'react'
 
-const EditForm = () => {
+const EditForm = ({params}) => {
+    const {user} = useUser();
+    const [jsonForm, setJsonForm] = useState([]);
+    useEffect(()=>{
+      user && GetFormData();
+    },[user])
+    const GetFormData=async()=>{
+        const result = await db.select().from(JsonForms)
+        .where(and(eq(JsonForms.id, params?.formId), eq(JsonForms.createdBy, user?.primaryEmailAddress?.emailAddress)))
+
+        console.log(result[0].jsonform);
+        setJsonForm(result[0].jsonform)
+    }
   return (
     <div>EditForm</div>
   )
