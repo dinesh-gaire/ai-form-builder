0d33a905de80404c4e915b397ce03a71752b2c02
dinesh-gaire <132206290+dinesh-gaire@users.noreply.github.com>
2024-05-25 09:06:19 +0000

    ui for form added

--- app/edit-form/[formId]/page.jsx
+++ app/edit-form/[formId]/page.jsx
@@ -3,11 +3,15 @@ import { db } from '@/configs'
 import { JsonForms } from '@/configs/schema'
 import { useUser } from '@clerk/nextjs'
 import { and, eq } from 'drizzle-orm'
+import { ArrowLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
+import FormUi from '../_components/FormUi'
 
 const EditForm = ({params}) => {
     const {user} = useUser();
     const [jsonForm, setJsonForm] = useState([]);
+    const router = useRouter();
     useEffect(()=>{
       user && GetFormData();
     },[user])
@@ -16,10 +20,22 @@ const EditForm = ({params}) => {
         .where(and(eq(JsonForms.id, params?.formId), eq(JsonForms.createdBy, user?.primaryEmailAddress?.emailAddress)))
 
         console.log(result[0].jsonform);
-        setJsonForm(result[0].jsonform)
+        setJsonForm(JSON.parse(result[0].jsonform))
     }
   return (
-    <div>EditForm</div>
+    <div className='p-10'>
+      <h2 className='flex gap-2 items-center my-5 cursor-pointer hover:font-bold' onClick={()=>router.back()}>
+        <ArrowLeft/> Back
+      </h2>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+        <div className='p-5 border rounded-lg shadow-md'>
+          Controller
+        </div>
+        <div className='md:col-span-2 border rounded-lg p-5 flex items-center justify-center'>
+          <FormUi jsonForm={jsonForm}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ app/edit-form/_components/FormUi.jsx
@@ -0,0 +1,89 @@
+import { Input } from '@/components/ui/input'
+import React from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { Checkbox } from "@/components/ui/checkbox"
+
+  
+
+const FormUi = ({jsonForm}) => {
+  return (
+    <div className='border p-5 md:w-[600px] rounded-lg'>
+        <h2 className='font-bold text-center text-2xl'>{jsonForm?.formTitle}</h2>
+        <h2 className='text-sm text-gray-400 text-center'>{jsonForm?.formHeading}</h2>
+        {jsonForm && jsonForm.fields && jsonForm.fields.length > 0 && (
+          jsonForm.fields.map((field, index) => (
+            <div key={index}>
+              {field?.fieldType=='select'?
+                <div  className='my-3'>
+                  <label className='text-xs text-gray-600'>{field?.label}</label>
+                  <Select>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder={field?.placeholder} />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {field?.options.map((option,index)=>(
+                        <SelectItem key={index} value={option}>{option}</SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </div>
+                :
+                field?.fieldType=='radio'?
+                <div>
+                  <label className='text-xs text-gray-600'>{field?.label}</label>
+                  <RadioGroup>
+                    {field?.options.map((option,index)=>(
+                      <div className="flex items-center space-x-2">
+                        <RadioGroupItem value={option?.label} id={option?.label} />
+                        <Label htmlFor={option?.label}>{option?.label}</Label>
+                      </div>
+                    ))}
+                  </RadioGroup>
+
+                </div>
+                :
+                field?.fieldType=='checkbox'?
+                <div className='my-3'>
+                  <label className='text-xs text-gray-600'>{field?.label}</label>
+                    {field?.options?field?.options?.map((option,index)=>(
+                      <div key={index} className='flex gap-2 items-center'>
+                        <Checkbox/>
+                        <h2>{option.label}</h2>
+                      </div>
+                    ))
+                    :
+                    <div className='flex gap-2 items-center'>
+                      <Checkbox/>
+                      <h2>{field?.label}</h2>
+                    </div>
+                    }
+                </div>
+                :
+                <div className='my-3'>
+                  <label className='text-xs text-gray-600'>{field?.label}</label>
+                  <Input
+                    type={field?.fieldType}
+                    placeholder={field?.placeholder}
+                    name={field?.fieldName}
+                  />
+                </div>
+              
+              }
+            </div>
+          ))
+        )}
+
+    </div>
+  )
+}
+
+export default FormUi
