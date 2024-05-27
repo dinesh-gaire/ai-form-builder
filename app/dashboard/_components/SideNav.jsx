8ad80adf5e6a142762b92ec781ee2aa8f5775345
dinesh-gaire <132206290+dinesh-gaire@users.noreply.github.com>
2024-05-27 15:18:12 +0000

    export form responses and save to excel functionality added

--- app/dashboard/_components/SideNav.jsx
+++ app/dashboard/_components/SideNav.jsx
@@ -4,6 +4,7 @@ import { usePathname } from 'next/navigation'
 import React from 'react'
 import { Button } from '../../../components/ui/button'
 import { Progress } from "../../../components/ui/progress"
+import Link from 'next/link'
 
 
 const SideNav = () => {
@@ -40,10 +41,10 @@ const SideNav = () => {
     <div className='h-screen shadow-md border'>
         <div className='p-5'>
             {menuList.map((menu, index)=>(
-                <h2 key={index} className={`flex items-center gap-3 p-4 mb-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer text-gray-500 ${path==menu.path && 'bg-primary text-white'}`}>
+                <Link href={menu.path} key={index} className={`flex items-center gap-3 p-4 mb-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer text-gray-500 ${path==menu.path && 'bg-primary text-white'}`}>
                     <menu.icon/>
                     {menu.name}
-                </h2>
+                </Link>
             ))}
         </div>
         <div className='fixed bottom-7 p-6 w-64'>
